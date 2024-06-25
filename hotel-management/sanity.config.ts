06a506f3bf8f662f08344efba9d82bfb24e8e30e
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-06-25 13:40:59 +0300

    feat: add Sanity Schemas for user, account, booking, hotelRoom, and verificationToke

--- hotel-management/sanity.config.ts
+++ hotel-management/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'hotel-management',
 
-  projectId: 'wppp4xpr',
-  dataset: 'hotel-management',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
 
   basePath: '/studio',
 
