569b52dfb86280c917455fe4b46409c63a9ee23f
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-06-26 21:57:50 +0300

    feat: add search feature

--- hotel-management/src/app/(web)/page.tsx
+++ hotel-management/src/app/(web)/page.tsx
@@ -1,8 +1,13 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
+import PageSearch from "@/components/PageSearch/PageSearch";
 
 const Home = () => {
   return <>
   <HeroSection/>
+  <PageSearch />
+  {/* Featured Room */}
+  {/* Gallery */}
+  {/* News Letter */}
   </>
 };
 
