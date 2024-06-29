b28fc9cfc388e2189ec96e574637afabc94c86c2
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-06-29 19:57:58 +0300

    feat: add Rooms Page

--- hotel-management/src/libs/apis.ts
+++ hotel-management/src/libs/apis.ts
@@ -1,13 +1,22 @@
-import { Room } from "@/app/models/room";
+import { Room } from "@/models/room";
 import sanityClient from "./sanity";
-import * as query from "./sanityQueries";
+import * as queries from "./sanityQueries";
 
 export async function getFeaturedRoom() {
         const result = await sanityClient.fetch<Room>(
-          query.getFeaturedRoomQuery, 
+          queries.getFeaturedRoomQuery, 
           {}, 
           {cache : 'no-cache'}
       );
 
       return result;
+}
+
+export async function getRooms() {
+  const result = await sanityClient.fetch<Room[]>(
+    queries.getRoomsQuery,
+    {},
+    { cache: 'no-cache' }
+  );
+  return result;
 }
